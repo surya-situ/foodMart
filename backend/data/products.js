9b9a1791d9c98107fe070a2ccef33451271827c1
Suryakanta Das <116926717+surya-situ@users.noreply.github.com>
2023-05-30 19:40:58 +0530

    all products

---
+++ backend/data/products.js
@@ -0,0 +1,100 @@
+const products = [
+  {
+    name: 'Hyadrabadi chicken biryani',
+    image: '/images/foodmart-biryani.jpg',
+    description:
+      'Authentic hydrabadi chicken biryani prepared with hand blend spices, slow cooked on low fire.',
+    brand: 'foodmart',
+    category: 'nonveg',
+    price: 180,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 3,
+  },
+  {
+    name: 'Veg cheese burger',
+    image: '/images/foodmart-veg-burger.jpg',
+    description:
+      ' cheesy garlic flavoured veg burger with sallow fried pattie prepared with tandoori meo.',
+    brand: 'foodmart',
+    category: 'veg',
+    price: 150,
+    countInStock: 20,
+    rating: 4.0,
+    numReviews: 4,
+  },
+  {
+    name: 'butter paneeer',
+    image: '/images/foodmart-paneer.jpg',
+    description:
+      'Paneer Butter Masala made with rich and creamy “makhani gravy” in tomato, butter and cashew paste with hand blend spices',
+    brand: 'foodmart',
+    category: 'veg',
+    price: 150,
+    countInStock: 25,
+    rating: 5,
+    numReviews: 2,
+  },
+  {
+    name: 'Masala dosa',
+    image: '/images/foodmart-dosa.jpg',
+    description:
+      'Healthy and crispy rava dosa with spicy and tangy coconut chatni',
+      brand: 'foodmart',
+    category: 'veg',
+    price: 80,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 3,
+  },
+  {
+    name: 'Logitech G-Series Gaming Mouse',
+    image: '/images/mouse.jpg',
+    description:
+      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+      brand: 'foodmart',
+    category: 'Electronics',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    name: 'Amazon Echo Dot 3rd Generation',
+    image: '/images/alexa.jpg',
+    description:
+      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+    brand: 'Amazon',
+    category: 'Electronics',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    name: 'Amazon Echo Dot',
+    image: '/images/alexa.jpg',
+    description:
+      'Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+    brand: 'Amazonion',
+    category: 'Electronics',
+    price: 29,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    name: 'Amazon Echo Dot',
+    image: '/images/alexa.jpg',
+    description:
+      'Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+    brand: 'Amazonion',
+    category: 'Electronics',
+    price: 29,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products;
